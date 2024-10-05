ed5f98f617c1d892cc97cf20f58025b7451b75b5
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-10-06 00:20:11 +0530

    Add files via upload

---
+++ biggest in 3.js
@@ -0,0 +1,26 @@
+var n1 = 10;
+var n2 = 5;
+var n3 = 11;
+
+if (n1 > n2) 
+    {
+    if (n1 > n3)
+         {
+        console.log("n1 is biggest");
+    } 
+    else
+     {
+        console.log("n3 is biggest");
+    }
+} 
+else
+ {
+    if (n2 > n3) 
+        {
+        console.log("n2 is biggest");
+    } 
+    else
+     {
+        console.log("n3 is biggest");
+    }
+}
