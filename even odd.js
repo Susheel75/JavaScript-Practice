280c0a8cfba7d1bd055b8338f29c219c98ee7aaf
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-10-08 00:57:35 +0530

    Add files via upload

---
+++ even odd.js
@@ -0,0 +1,10 @@
+var n = 7;
+if (number % 2 === 0) 
+{
+  console.log("The number is even.");
+} 
+else 
+{
+  console.log("The number is odd.");
+}
+// https://medium.com/@andrei_diaconu/it-beginner-series-javascript-if-else-exercises-cfc5d65b6f94#:~:text=JavaScript%20offers%20the%20if%2Felse,whether%20those%20conditions%20are%20met.
