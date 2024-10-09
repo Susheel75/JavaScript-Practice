2f0717cd5ed0059e1e203b956dc192a13be921d9
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-10-09 14:26:09 +0530

    Add files via upload

---
+++ grade.js
@@ -0,0 +1,23 @@
+var mark = 85;
+var grade;
+if(mark>=85)
+{
+    console.log("Grade A");
+    
+}
+else if(mark>=80)
+{
+    console.log("Grade B")
+}
+else if(mark>=75)
+{
+    console.log("Grade C")
+}
+else if(mark>=70)
+{
+    console.log("Grade D")
+}
+else
+{
+    console.log("F")
+}
