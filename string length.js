d85bd62d38da9a2b06459496db3bc1e7b14e07dc
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-10-12 00:46:50 +0530

    Add files via upload

---
+++ string length.js
@@ -0,0 +1,16 @@
+// Task
+// Write a program to check whether the length of the string CodeChef is less than 5 or greater.
+
+// If the length is greater than 5 print Long String
+// If it's less than or equal to 5 print Short String
+
+
+// your code goes here4
+var text = "CodeChef";
+var textLength = text.length;
+
+if(textLength > 5) {
+  console.log("Long String"); 
+} else {
+  console.log("Short String"); 
+}
