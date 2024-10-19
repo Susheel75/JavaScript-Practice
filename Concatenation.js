6c919bb36be9f29fb5f8812d8204d41969ce9afa
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-10-19 12:09:42 +0530

    Add files via upload

---
+++ Concatenation.js
@@ -0,0 +1,25 @@
+// Concatenation
+// The '+' sign can be used between strings to add them together to make a new string.
+// This is called concatenation.
+// For eg -
+
+// var x = "Good";
+// var y = "Work";
+// console.log(x + y);   //output: GoodWork
+// Note: You can add spaces between words by using an empty " " with a space inside or by providing a space at the end of a word.
+// For eg -
+
+// var x = "Good";
+// var y = "Work";
+// console.log(x + " " + y);   // output: Good Work
+// Task
+// Write a program which does the following
+
+// Declare two variables x and y
+// Assign the values Hello to variable x and World to variable y.
+// Output x and y to the console. Don't forget to add a space in between them.
+// Your output will be Hello World
+
+var x = "Hello";
+var y = "World";
+console.log(x + " " + y);   // output: Hello World
