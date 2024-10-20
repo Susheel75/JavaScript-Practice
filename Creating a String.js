750054e8b93a999c717b9936cbdddffc99aa9ce7
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-10-20 22:13:31 +0530

    Add files via upload

---
+++ Creating a String.js
@@ -0,0 +1,15 @@
+// Creating a String
+// JavaScript is a versatile programming language, and one of its fundamental data types is the string. A string in JavaScript is a sequence of characters enclosed within single (' '), double (" "), or backticks (`) quotes. Whether you're manipulating text, building user interfaces, or performing data validation, understanding JavaScript strings is crucial. In this section, we'll delve into the basics and explore some common operations and methods related to strings.
+
+// For example:
+
+// var singleQuotedString = 'Hello, World!';
+// var doubleQuotedString = "JavaScript is fun!";
+// var backticksString = `You can use backticks too!`;
+// Write a program which does the following
+
+// Create a string named fruit and assign the value Apple to it.
+// Output fruit to the console.
+
+var fruit = "Apple";
+console.log(fruit);
