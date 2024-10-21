c40dc7feadf8a406883de55ecd201f0cb17cc1d6
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-10-21 19:12:46 +0530

    Add files via upload

---
+++ String length.js
@@ -0,0 +1,19 @@
+
+// String length
+// We can use the length property to get the length of a string.
+// For eg.
+
+// var greeting = "hello";
+// console.log(greeting.length);
+// // output is 5
+// Task
+// Write a program which does the following
+
+// Create a variable txt and assign it the text NumeroTres
+// Use the length property to output to the console the number of characters in txt
+
+// 
+
+var txt = "NumeroTres";
+var txtLen = txt.length;
+console.log(txtLen);
