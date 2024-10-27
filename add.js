6182b245a686135da6c079a85605c08cd4e80cb4
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-10-27 22:11:42 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,21 @@
+// Addition
+
+var x=2;
+var y=3;
+function add(x,y)
+{
+    var ans=x+y;
+    return ans;
+}
+console.log(add(x,y));
+
+// Subtraction
+
+var a=4;
+var b=1;
+function subtract(a,b)
+{
+    var res=a-b;
+    return res;
+}
+console.log(subtract(a,b));
