f729a6fb2058c28812cfd4a8392cf8a2d115bc78
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-11-04 15:13:24 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,20 @@
+var grade=30;
+switch(true){
+    case grade>40:
+        console.log("A")
+        break;
+    case grade>30:
+        console.log("B")
+        break;
+    case grade>20:
+        console.log("C")
+        break;
+    case grade>10:
+        console.log("D")
+        break;
+    case grade>0:
+        console.log("F")
+        break;
+    default:
+        console.log("Invalid grade")
+}
