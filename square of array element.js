b919d78f9f75f3064e155704e15d02ab4cb715ae
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-11-09 12:40:26 +0530

    Add files via upload

---
+++ square of array element.js
@@ -0,0 +1,20 @@
+var arr = [1,3,5,7];
+var newArr = [];
+for (var i = 0; i < arr.length; i++)
+{
+    var e = arr[i];
+    newArr.push(e*e);
+}
+console.log(newArr);
+
+
+
+
+var arr = [1,3,5,7];
+var square = [];
+for (var i = 0; i < arr.length; i++)
+{
+    square.push(arr[i]*arr[i]);
+
+}
+console.log(square);
