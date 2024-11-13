3514ae916f6332e81a1a9dfb904abf3b39206564
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-11-13 21:17:06 +0530

    Add files via upload

---
+++ change index value.js
@@ -0,0 +1,18 @@
+// const arr = [2,1,5,7];
+// console.log(arr)
+// arr[1]=30; // change index value of 1
+// console.log(arr);
+
+/*const x =  [2,3,5,6];
+x = [2,8,9]
+console.log(x); // this will throw an error because you are trying to reassign a value to
+// a constant variable*/
+
+var arr = [1,2,3,4,5,6,7,[8,9,10],[11,12,13]]
+var arr1=arr.length;
+console.log(arr1)
+console.log(arr[7]);
+arr[8].push(14);
+console.log(arr);
+
+// console.log((arr));
