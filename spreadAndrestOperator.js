0b72a38e4c872cd2fa2e6ea47f3f86e432dfba4f
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-11-18 14:04:34 +0530

    Update spreadAndrestOperator.js

--- spreadAndrestOperator.js
+++ spreadAndrestOperator.js
@@ -1,53 +1,53 @@
-// function sum(...arr)
-// {
-//     var s=0;
-//     if(arr.length!=0)
-//     {
-//         for(var i=0;i<arr.length;i++)
-//         {
-//             s+= arr[i]
-//         }
-//     }
-//     console.log(arr,s);
-
-// }
-// sum();
-// sum(1,2);
-// sum(1,2,3);
-// sum(10,100,1);
-//               // [] 0
-// //output      // [ 1, 2 ] 3 
-//               // [ 1, 2, 3 ] 6
-//               // [ 10, 100, 1 ] 111
-
-
-function charCount(data){
-    var newStr='';
-    var count=1;
-    var char=''
-    for(var i=0;i<data.length;i++){
-        var e=data[i];
-        if(char==''){
-            char=e;
-            newStr=e;
-        }
-        else{
-            if(char==e){
-                count++;
-            }
-            else{
-                
-                newStr+=count;
-                newStr+=e;
-                count=1
-                char=e;
-            }
+function sum(...arr)
+{
+    var s=0;
+    if(arr.length!=0)
+    {
+        for(var i=0;i<arr.length;i++)
+        {
+            s+= arr[i]
         }
     }
-    newStr+=count;
-    console.log(newStr)
+    console.log(arr,s);
+
 }
-charCount('aabbbbbbbcccccaaa');
+sum();
+sum(1,2);
+sum(1,2,3);
+sum(10,100,1);
+              // [] 0
+//output      // [ 1, 2 ] 3 
+              // [ 1, 2, 3 ] 6
+              // [ 10, 100, 1 ] 111
+
+
+// function charCount(data){
+//     var newStr='';
+//     var count=1;
+//     var char=''
+//     for(var i=0;i<data.length;i++){
+//         var e=data[i];
+//         if(char==''){
+//             char=e;
+//             newStr=e;
+//         }
+//         else{
+//             if(char==e){
+//                 count++;
+//             }
+//             else{
+                
+//                 newStr+=count;
+//                 newStr+=e;
+//                 count=1
+//                 char=e;
+//             }
+//         }
+//     }
+//     newStr+=count;
+//     console.log(newStr)
+// }
+// charCount('aabbbbbbbcccccaaa');
 
 // var x=10;
 // (function (){
