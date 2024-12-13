2439ff67a8eeb3e29825476d65eef5a907ac14eb
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-12-13 16:31:07 +0530

    Add files via upload

---
+++ Synchronus.js
@@ -0,0 +1,17 @@
+
+1.
+console.log("Start");
+console.log("Task 1:");
+console.log("Task 2:");
+console.log("Task 3:");
+console.log("End");
+
+2.
+console.log("")
+console.log("Start");
+for(var i=1; i<=5; i++)
+{
+    console.log(i);
+    
+}
+console.log("End")
