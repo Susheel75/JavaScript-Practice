ed75ad30dfd1849c979ef82494d7e7e7f5cb5a9f
Susheel Prajapati <85658086+Susheel75@users.noreply.github.com>
2024-12-17 21:17:02 +0530

    Add files via upload

---
+++ [^abc].js
@@ -0,0 +1,6 @@
+var regex=/[^abc]/;
+console.log(regex.test("amazon"));
+console.log(regex.test("apple"));
+console.log(regex.test("dog"));
+console.log(regex.test("susheel"));
+console.log(regex.test("abc"));
